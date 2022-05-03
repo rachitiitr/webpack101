d7892d6239137e91b757c67cf74f95b853b9f2ba
Rachit Jain <rachit.jain2095@gmail.com>
2022-05-03 08:44:42 +0000

    chapter6: Basic Setup for UI component Demo

---
+++ src/WelcomeMessage/WelcomeMessage.js
@@ -0,0 +1,8 @@
+/**
+ * 
+ * @param {{person: string}} param0 
+ * @returns 
+ */
+export function WelcomeMessage({person}) {
+    return `Hi ${person.toUpperCase()}, great to see you learn Webpack!`;
+}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import {add, mult} from "./math";
+import { WelcomeMessage } from "./WelcomeMessage";
 
 console.log('Hello, learning webpack');
 console.log(add(3,4));
 console.log(add(13,4));
 console.log(mult(13,4));
 console.log(mult(13,4));
+
+const WelcomeMessageDiv = WelcomeMessage({person: "rachit"});
+console.log(WelcomeMessageDiv);
